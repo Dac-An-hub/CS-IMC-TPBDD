92fdf1f765f03b12ff7b6747e9380d593a3815ab
Dac-An-hub <dacanvo@gmail.com>
2025-01-14 16:04:17 +0000

    TP1

--- export-neo4j copy.py
+++ export-neo4j copy.py
@@ -1,4 +1,5 @@
 import os
+
 import dotenv
 import pyodbc
 from py2neo import Graph
@@ -11,7 +12,7 @@ server = os.environ["TPBDD_SERVER"]
 database = os.environ["TPBDD_DB"]
 username = os.environ["TPBDD_USERNAME"]
 password = os.environ["TPBDD_PASSWORD"]
-driver = os.environ["ODBC_DRIVER"]
+driver= os.environ["ODBC_DRIVER"]
 
 neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
 neo4j_user = os.environ["TPBDD_NEO4J_USER"]
@@ -26,9 +27,7 @@ graph.run("MATCH ()-[r]->() DELETE r")
 graph.run("MATCH (n:Artist) DETACH DELETE n")
 graph.run("MATCH (n:Film) DETACH DELETE n")
 
-with pyodbc.connect(
-    f'DRIVER={driver};SERVER=tcp:{server};PORT=1433;DATABASE={database};UID={username};PWD={password}'
-) as conn:
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
     cursor = conn.cursor()
 
     # Films
@@ -42,10 +41,13 @@ with pyodbc.connect(
         if not rows:
             break
 
+        i = 0
         for row in rows:
             # Créer un objet Node avec comme label Film et les propriétés adéquates
-            n = Node("Film", idFilm=row[0], primaryTitle=row[1], startYear=row[2])
+            # A COMPLETER
+            n = Node("Artist", idArtist=row[0], primaryName=row[1])
             importData.append(n)
+            i += 1
 
         try:
             create_nodes(graph.auto(), importData, labels={"Film"})
@@ -55,59 +57,41 @@ with pyodbc.connect(
             print(error)
 
     # Names
-    exportedCount = 0
-    cursor.execute("SELECT COUNT(1) FROM TArtist")
-    totalCount = cursor.fetchval()
-    cursor.execute("SELECT idArtist, primaryName, birthYear FROM TArtist")
-    while True:
-        importData = []
-        rows = cursor.fetchmany(BATCH_SIZE)
-        if not rows:
-            break
-
-        for row in rows:
-            # Créer un objet Node avec comme label Artist et les propriétés adéquates
-            n = Node("Artist", idArtist=row[0], primaryName=row[1], birthYear=row[2])
-            importData.append(n)
-
-        try:
-            create_nodes(graph.auto(), importData, labels={"Artist"})
-            exportedCount += len(rows)
-            print(f"{exportedCount}/{totalCount} artist records exported to Neo4j")
-        except Exception as error:
-            print(error)
+    # En vous basant sur ce qui a été fait dans la section précédente, exportez les données de la table tNames
+    # A COMPLETER
 
     try:
         print("Indexing Film nodes...")
-        graph.run("CREATE INDEX FOR (f:Film) ON (f.idFilm)")
-        print("Indexing Artist nodes...")
-        graph.run("CREATE INDEX FOR (a:Artist) ON (a.idArtist)")
+        graph.run("CREATE INDEX ON :Film(idFilm)")
+        print("Indexing Name (Artist) nodes...")
+        graph.run("CREATE INDEX ON :Artist(idArtist)")
     except Exception as error:
         print(error)
+
 
     # Relationships
     exportedCount = 0
     cursor.execute("SELECT COUNT(1) FROM tJob")
     totalCount = cursor.fetchval()
-    cursor.execute("SELECT idArtist, category, idFilm FROM tJob")
+    cursor.execute(f"SELECT idArtist, category, idFilm FROM tJob")
     while True:
-        importData = {"acted_in": [], "directed": [], "produced": [], "composed": []}
+        importData = { "acted in": [], "directed": [], "produced": [], "composed": [] }
         rows = cursor.fetchmany(BATCH_SIZE)
         if not rows:
             break
 
         for row in rows:
-            relTuple = (row[0], {}, row[2])
-            category = row[1].replace(" ", "_").lower()
-            if category in importData:
-                importData[category].append(relTuple)
+            relTuple=(row[0], {}, row[2])
+            importData[row[1]].append(relTuple)
 
         try:
-            for cat, relationships in importData.items():
-                if relationships:
-                    create_relationships(
-                        graph.auto(), relationships, cat, ("Artist", "idArtist"), ("Film", "idFilm")
-                    )
+            for cat in importData:
+                # Utilisez la fonction create_relationships de py2neo pour créer les relations entre les noeuds Film et Name
+                # (les tuples nécessaires ont déjà été créés ci-dessus dans la boucle for précédente)
+                # https://py2neo.org/2021.1/bulk/index.html
+                # ATTENTION: remplacez les espaces par des _ pour nommer les types de relation
+                # A COMPLETER
+                None # Remplacez None par votre code
             exportedCount += len(rows)
             print(f"{exportedCount}/{totalCount} relationships exported to Neo4j")
         except Exception as error:
